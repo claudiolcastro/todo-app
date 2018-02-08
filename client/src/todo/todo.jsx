14508248b535f83134698b32d0227f218ce4d049
Claudio Castro <claudioluiz.castro@gmail.com>
2018-02-08 19:31:26 -0200

    Add handles to todoForm

--- client/src/todo/todo.jsx
+++ client/src/todo/todo.jsx
@@ -5,11 +5,30 @@ import TodoForm from '../todo/todoForm'
 import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+  
+  handleAdd() {
+    console.log(this.state.description)
+  }
+
   render() {
     return(
       <div>
         <PageHeader name="Tarefas" small="cadastro" />
-        <TodoForm />
+        <TodoForm 
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     )
