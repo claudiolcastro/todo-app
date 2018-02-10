df4169038ad47cb508f01ec35fa9f25541d217f4
Claudio Castro <claudioluiz.castro@gmail.com>
2018-02-10 21:23:59 -0200

    Add remove button

--- client/src/todo/todo.jsx
+++ client/src/todo/todo.jsx
@@ -14,6 +14,9 @@ export default class Todo extends Component {
 
     this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+
+    this.refresh()
   }
 
   handleChange(e) {
@@ -23,18 +26,32 @@ export default class Todo extends Component {
   handleAdd() {
     const description = this.state.description
     axios.post(URL, { description })
-         .then(resp => console.log(`${description} added to database`))
+         .then(resp => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+         .then(resp => this.refresh())
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+         .then(resp => this.setState({...this.state, description: '', list: resp.data}))
   }
 
   render() {
     return(
       <div>
         <PageHeader name="Tarefas" small="cadastro" />
-        <TodoForm 
+        <TodoForm
+          description={this.state.description}
           handleAdd={this.handleAdd}
           handleChange={this.handleChange}
         />
-        <TodoList />
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+        />
       </div>
     )
   }
