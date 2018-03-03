22eaef51a0357f5b8801866d5646d1a7b41a48ac
Claudio Castro <claudioluiz.castro@gmail.com>
2018-03-03 19:20:28 -0300

    Add Search Action

--- client/src/todo/todoActions.jsx
+++ client/src/todo/todoActions.jsx
@@ -1,6 +1,18 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
+
 const changeDescription = event => ({
   type: 'DESCRIPTION_CHANGED',
   payload: event.target.value
 })
 
-export { changeDescription }
+const search = () => {
+  const request = axios.get(`${URL}?sort=-createdAt`)
+  return {
+    type: 'TODO_SEARCHED',
+    payload: request
+  }
+}
+
+export { changeDescription, search }
--- client/src/todo/todoForm.jsx
+++ client/src/todo/todoForm.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
-  return (
-    <div role='form' className='todoForm'>
-      <Grid cols='12 9 10'>
-        <input
-          id='description'
-          className='form-control'
-          placeholder='Adicione uma tarefa'
-          value={props.description}
-          onChange={props.changeDescription}
-        />
-      </Grid>
 
-      <Grid cols='12 3 2'>
-        <IconButton style='primary' icon='plus' onClick={props.handleAdd}/>
-        <IconButton style={`info ${css(styles.buttonToLeft)}`} icon='search' onClick={props.handleSearch} />
-        <IconButton style={`default ${css(styles.buttonToLeft)}`} icon='close' onClick={props.handleClear} />
-      </Grid>
-    </div>
-  )
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    this.props.search()
+  }
+
+  render() {
+    return(
+      <div role = 'form' className = 'todoForm' >
+        <Grid cols='12 9 10'>
+          <input
+            id='description'
+            className='form-control'
+            placeholder='Adicione uma tarefa'
+            value={this.props.description}
+            onChange={this.props.changeDescription}
+          />
+        </Grid>
+        
+        <Grid cols='12 3 2'>
+          <IconButton style='primary' icon='plus' onClick={this.props.handleAdd} />
+          <IconButton style={`info ${css(styles.buttonToLeft)}`} icon='search' onClick={this.props.handleSearch} />
+          <IconButton style={`default ${css(styles.buttonToLeft)}`} icon='close' onClick={this.props.handleClear} />
+        </Grid>
+      </div>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -37,6 +48,6 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ changeDescription }, dispatch)
+  bindActionCreators({ changeDescription, search }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
